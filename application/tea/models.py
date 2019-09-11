d592d66c5e4836d0082109fb18f0e8dc1992b107
Olli Määttä <ollimaatta@gmail.com>
2019-09-11 20:06:47 +0300

    tietokanta lisätty

--- application/tea/models.py
+++ application/tea/models.py
@@ -1,30 +1,30 @@
 from application import db
 
-class Ingredient(db.model):
+class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128), nullable=False)
 
     def __init__(self, name):
         self.name = name
 
-class Type(db.model):
+class Type(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), nullable=False)
 
     def __init__(self, name):
         self.name = name
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128), nullable=False)
 
     def __init__(self, name):
         self.name = name
 
-class Review(db.model):
+class Review(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128), nullable=False)
-    content = db.Column(db.String(512), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.Text)
 
     def __init__(self, name):
         self.name = name
