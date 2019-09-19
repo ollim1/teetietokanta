986c92b386abc8cfd1003353611a5dd75d033762
Olli Määttä <ollimaatta@gmail.com>
2019-09-19 21:46:51 +0300

    käyttäjien lisääminen toteutettu alustavasti

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField
   
+class AddUserForm(FlaskForm):
+    name = StringField("Koko nimi")
+    username = StringField("Käyttäjänimi")
+    password = PasswordField("Salasana")
+  
+    class Meta:
+        csrf = False
+
 class LoginForm(FlaskForm):
     username = StringField("Käyttäjä")
     password = PasswordField("Salasana")
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, current_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, AddUserForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -24,4 +24,23 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/auth/add_user", methods = ["GET", "POST"])
+def auth_add_user():
+    if request.method == "GET":
+        return render_template("auth/add_user.html", form = AddUserForm())
+
+    form = AddUserForm(request.form)
+
+    user = User.query.filter_by(username=form.username.data).first()
+    if user:
+        return render_template("auth/add_user.html", form = form,
+                               error = "Käyttäjänimi on jo käytössä")
+
+    user = User(form.name.data, form.username.data, form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    print("Käyttäjä " + user.name + " luotu")
+    login_user(user)
     return redirect(url_for("index"))
