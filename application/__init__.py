33a02646ef976cc294a9f7700ca3fa0a9b59256d
Olli Määttä <ollimaatta@gmail.com>
2019-09-21 12:58:39 +0300

    postgresql otettu käyttöön

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tea.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+# using postgresql on heroku
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tea.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
-# sovellus
+# application
 from application import views
 
 from application.tea import models
@@ -16,7 +22,7 @@ from application.tea import views
 from application.auth import models
 from application.auth import views
 
-# kirjautuminen
+# login
 from application.auth.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -32,4 +38,7 @@ login_manager.login_message = "Kirjaudu sisään käyttääksesi tätä toiminto
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
