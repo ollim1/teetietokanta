ebb1b36d4020b72514519fad9168b1537d633896
Olli Määttä <ollimaatta@gmail.com>
2019-10-13 17:02:15 +0300

    salasanan tarkistus lisätty rekisteröitymislomakkeeseen

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -5,6 +5,7 @@ class AddUserForm(FlaskForm):
     name = StringField("Koko nimi", [validators.Length(min=1, max=143)])
     username = StringField("Käyttäjänimi", [validators.Length(min=1, max=143)])
     password = PasswordField("Salasana", [validators.Length(min=1, max=143)])
+    password_check = PasswordField("Sanasanan tarkistus", [validators.Length(min=1, max=143), validators.equal_to("password", message="Salasanojen on oltava sama")])
   
     class Meta:
         csrf = False
